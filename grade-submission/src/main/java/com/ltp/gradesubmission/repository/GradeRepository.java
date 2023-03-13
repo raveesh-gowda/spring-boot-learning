81c6f20962884864f92cfff279ed000527f5c6e6
raveesh.gowda <raveesh.gowda@costrategix.com>
2023-03-13 13:37:23 +0530

    beans and dependency injection

---
+++ grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -0,0 +1,31 @@
+package com.ltp.gradesubmission.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ltp.gradesubmission.Grade;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GradeRepository {
+
+    private final List<Grade> studentGrades = new ArrayList<>();
+
+    public Grade getGrade(int index) {
+        return studentGrades.get(index);
+    }
+
+    public void addGrade(Grade grade) {
+        studentGrades.add(grade);
+    }
+
+    public void updateGrade(Grade grade, int index) {
+        studentGrades.set(index, grade);
+    }
+
+    public List<Grade> getGrades() {
+        return studentGrades;
+    }
+
+}
